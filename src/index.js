6fe97ad53aaf32502092e759cc2cda461a3eaa69
matvey-nyashka <matvej.gnedin.10@mail.ru>
2021-08-23 22:11:02 +0300

    feat: done morse-decoder

--- src/index.js
+++ src/index.js
@@ -35,11 +35,35 @@ const MORSE_TABLE = {
     '---..':  '8',
     '----.':  '9',
     '-----':  '0',
-};
-
-function decode(expr) {
-    // write your solution here
-}
+    ' ': ' '
+  };
+  
+  function decode(expr) {
+      let str = expr.match(/.{10}/g);
+      let decodeString = '';
+      let k = '';
+      let arr = str.map(function(name){
+       return name.match(/.{2}/g)
+      })
+  
+      for (let i = 0; i < arr.length; i++) {
+        k = '';
+        for (let j = 0; j < 5; j++){
+          if (arr[i][j] == '00'){
+            continue
+          } else if (arr[i][j] == '10'){
+            k += '.'
+          } else if (arr[i][j] == '11'){
+            k += '-'
+          } else {
+            k = ' '
+            break;
+          }
+        }
+        decodeString += MORSE_TABLE[k]
+      }
+      return decodeString;
+  }
 
 module.exports = {
     decode
